1907c53e372562844e5577e371b7df9b60628c78
Safeya-Yasien <ssafeya5@gmail.com>
2025-03-22 22:20:58 +0200

    delete 1 file and update 1 file udpate vite.config.js to ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -13,6 +12,7 @@ export default defineConfig({
       "@redux": path.resolve(__dirname, "./src/redux"),
       "@routes": path.resolve(__dirname, "./src/routes"),
       "@layouts": path.resolve(__dirname, "./src/layouts"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
     },
   },
 });
