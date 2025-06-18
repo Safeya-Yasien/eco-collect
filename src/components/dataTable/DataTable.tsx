9798c605c8d0bb30774c76107d239489a2e08f5f
Safeya-Yasien <ssafeya5@gmail.com>
2025-06-18 08:39:22 +0300

    refactor: simplify DataTable component by removing lazy loading and suspense

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -1,9 +1,4 @@
-import { GridColDef, GridToolbar } from "@mui/x-data-grid";
-import { lazy, Suspense } from "react";
-
-const DataGrid = lazy(() =>
-  import("@mui/x-data-grid").then((mod) => ({ default: mod.DataGrid }))
-);
+import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
 
 type TDataTableProps = {
   columns: GridColDef[];
@@ -14,11 +9,9 @@ type TDataTableProps = {
 
 const DataTable = ({ columns, rows }: TDataTableProps) => {
   return (
-    <Suspense
-      fallback={<div className="text-center py-10">Loading table...</div>}
-    >
+    <>
       <DataGrid
-        className="!bg-white !border-[2px] !border-[#B0BEC5] !shadow-[4px_4px_4px_0px_#00000040] !rounded-xl relative"
+        className="!bg-white !border-[2px] !border-[#B0BEC5] !shadow-[4px_4px_4px_0px_#00000040] !rounded-xl relative "
         rows={rows}
         columns={columns}
         initialState={{
@@ -35,8 +28,7 @@ const DataTable = ({ columns, rows }: TDataTableProps) => {
         disableColumnSelector
         slots={{ toolbar: GridToolbar }}
       />
-    </Suspense>
+    </>
   );
 };
-
 export default DataTable;
